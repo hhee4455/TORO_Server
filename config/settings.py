2d9ea4fd3862520b1b40bdd38f5646142c2bb8f0
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-10-24 15:10:31 +0900

    Fix : 멀티모듈, 클린아키텍처 구조 수정

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # postresql database
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toro_db',
+        'USER ': 'toro_admin',
+        'PASSWORD': 'toro_pwd123!',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
